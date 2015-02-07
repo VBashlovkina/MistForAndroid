2e67b71d8b6e72a9da1d54f466141c52d14c9d95
Vasilisa Bashlovkina <bashlovk@grinnell.edu>
2015-02-07 14:38:55 -0600

    fixed peek and next combined behavior

--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -18,13 +18,13 @@ public class Tokenizer {
     }
 
     public String next() {
-
-	
+	if (peeked.isEmpty())
+	    this.peek();
 	index += peeked.length();
-	return this.peek();
+	return peeked;
     }
 
-    // TODO deal with whitespace!
+   
     public String peek() {
 	// clear peeked
 	peeked = "";
@@ -36,25 +36,25 @@ public class Tokenizer {
 	    if (c == '(' || c == ')' || c == ',')
 		return (peeked = c + "");
 	    // otherwise, parse a word token
-	    while (index + i < code.length - 1
-		    && (c = code[index + i]) != '(' && c != ')' && c != ',') {
-		// discard whitespace but increment index to account for skipping
+	    while (index + i < code.length - 1 && (c = code[index + i]) != '('
+		    && c != ')' && c != ',') {
+		// discard whitespace but increment index to account for
+		// skipping
 		if (Character.isWhitespace(c))
 		    index++;
-		else
-		{
-		 peeked += c;
-		 i++;
+		else {
+		    peeked += c;
+		    i++;
 		}
 	    }// while reading a word token
-	}// if has
+	}// if
 	return peeked;
-    }
+    }// peek
 
-    public String toString()
-    {
+    public String toString() {
 	return new String(code) + " with peeked: " + peeked;
     }
+
     /**
      * @param args
      */
@@ -62,11 +62,11 @@ public class Tokenizer {
 	String test = "rgb(sq(sum(x,y)),x,y)";
 	String okay = "rgb(wsum(x, y, y, t.s), sum(sum(wsum(x, x, y, neg(t.s), x), wsum(y, y, x, neg(t.s), y)), sum(wsum(y, x, x, t.s), wsum(x, y, y, t.s))), sum(wsum(x, x, y, neg(t.s), x), wsum(y, y, x, neg(t.s), y)))";
 	String zoes = "mult(sum(cos(square(sum(sin(x), cos(mult(t.m, 5))))), cos(square(sum(sin(y), cos(mult(t.m, 5)))))), 10)";
-	Tokenizer t = new Tokenizer(zoes);
+	Tokenizer t = new Tokenizer(test);
 	PrintWriter pen = new PrintWriter(System.out, true);
-	while (t.hasNext())
-	{
-	    pen.println(t.next());
+	while (t.hasNext()) {
+	    pen.println("Peeked at: " + t.peek());
+	    pen.println(" Next is: " + t.next());
 	}
     }// main
 
