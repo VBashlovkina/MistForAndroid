12c5612b9c34ff33f84b10744cf60e0b3567dceb
boatenga17 <boatenga17@grinnell.edu>
2015-02-13 19:17:57 -0600

    Reorganize classes into package. Add getter for the value of the node ( getTreeVal() )

---
+++ src/edu/grinnell/glimmer/mist/parser/TreeNode.java
@@ -0,0 +1,97 @@
+package edu.grinnell.glimmer.mist.parser;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+
+public class TreeNode {
+
+    String val;
+    ArrayList<TreeNode> children;
+
+    /* Constructor */
+    
+    public TreeNode(String str) {
+	val = str;
+	children = new ArrayList<>();
+    }// TreeNode
+
+    
+    /* Adding children */ 
+    
+    public void addChild(String child) {
+	this.children.add(new TreeNode(child));
+    }// addChild
+
+    public void addChild(TreeNode child) {
+	this.children.add(child);
+    }
+    
+    /* Getters */
+    
+    public int numChildren() {
+	return children.size();
+    }// numChildren
+
+    public boolean hasChildren() {
+	if (children.size() > 0)
+	    return true;
+	return false;
+    }// hasChildren
+
+    public ArrayList<TreeNode> getChildren() {
+	return this.children;
+    }
+    
+    public String getTreeVal(){
+      return this.val;
+    }
+    /* Utility */
+    
+    // TODO 
+    public boolean equals(TreeNode other)
+    {
+	return this.toString().equals(other.toString());
+    } //hacked equals
+    
+    public String toString() {
+	return toStringHelper(this, "");
+    }// toString
+
+    String toStringHelper(TreeNode root, String indent) {
+	String result = indent + root.val + "\n";
+	// result += indent + root.val + "\n";
+	if (root.hasChildren()) // not a leaf
+	{
+	    indent += "  ";
+	    for (TreeNode t : root.children) {
+		result += toStringHelper(t, indent);
+	    }
+	}
+	return result;
+    }// toString Helper
+
+    
+    
+    public static void main(String[] args) throws Exception {
+	TreeNode result = new TreeNode("sum");
+	result.addChild("x");
+	result.addChild("y");
+	PrintWriter pen = new PrintWriter(System.out, true);
+	pen.println(result);
+	String test = "rgb(sq(sum(x,y)),x,y)";
+	String okay = "rgb(wsum(x, y, y, t.s), sum(sum(wsum(x, x, y, neg(t.s), x), wsum(y, y, x, neg(t.s), y)), sum(wsum(y, x, x, t.s), wsum(x, y, y, t.s))), sum(wsum(x, x, y, neg(t.s), x), wsum(y, y, x, neg(t.s), y)))";
+	TreeNode t = Parser.parse(okay);
+	String recParsed = t.toString();
+	String itParsed = AltParser.parse(okay).toString();
+	System.out.println(recParsed.equals(itParsed));
+	System.out.println(recParsed);
+	System.out.println(itParsed);
+	
+	//Parser.parse("");
+	TreeNode res = Parser.parse("x");
+	TreeNode correct = new TreeNode("x");
+	System.out.println(res);
+	
+    }
+
+}
