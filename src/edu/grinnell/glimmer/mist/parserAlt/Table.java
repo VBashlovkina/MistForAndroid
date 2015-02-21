4cecd511699dde9f1c4b598ba79d7f38d5e06fcc
albertowusuasare <owusuasa@grinnell.edu>
2015-02-20 22:14:29 -0500

    -> Create Junit Test for Table -> Add getMap() in Table

--- src/edu/grinnell/glimmer/mist/parserAlt/Table.java
+++ src/edu/grinnell/glimmer/mist/parserAlt/Table.java
@@ -1,85 +1,69 @@
 package edu.grinnell.glimmer.mist.parserAlt;
-import java.io.PrintWriter;
 import java.util.HashMap;
 
 public class Table
 {
-   /**
-   * Encapsulates a HashMap and an Integer counter
-   * @author Albert Owusu-Asare
-   * @since 1.0
+  /**
+  * Encapsulates a HashMap and an Integer counter
+  * @author Albert Owusu-Asare
+  * @since 1.0
+  */
+
+  HashMap<HashNode, Integer> map;
+  Integer availableNumber;
+  HashNode rootNode;
+
+  /**
+   * Constructs a <code>table</code>
+   * @param map a HashMap of key <code>HashNode</code> value:
+   *            <code>Integer</code>
    */
- 
-    HashMap<HashNode, Integer> map;
-    
-    Integer availableNumber;
+  public Table(HashMap<HashNode, Integer> map)
+  {
+    this.map = map;
+    this.availableNumber = 1;
+    this.rootNode = null;
+  }// Table(HashMap)
+  
+  /**
+   * Sets the rootNode
+   * @param rootNode
+   */
+  public void setRootNode(HashNode rootNode)
+  {
+    this.rootNode = rootNode;
+  }//setRootNode(HashNode)
+  
+  /**
+   * Increments the availableNumber counter to help associate 
+   * values for keys in HashMap.
+   */
+  public void incrementAvailableNum()
+  {
+    this.availableNumber++;
+  }//incrementAvailableNum()
 
-    private HashNode rootNode;
-    /**
-     * Constructs a <code>table</code>
-     * @param map a HashMap of key <code>HashNode</code> value:
-     *            <code>Integer</code>
-     */
-    public Table(HashMap<HashNode, Integer> map)
-    {
-      this.map = map;
-      this.availableNumber = 1;
-      this.rootNode = null;
-    }// Table
+  /**
+   * Gets the rootNode
+   * @return
+   */
+  public HashNode getRootNode()
+  {
+    return this.rootNode;
+  }//getRootNode()
 
-    /**
-     * Increments the available Number
-     */
-
-    public void incrementAvailableNum()
-    {
-      this.availableNumber++;
-    }//
-
-    /**
-     * Gets the rootNode
-     * @return
-     */
-    public HashNode getRootNode(){
-      return this.rootNode;
-    }
-    
-    /**
-     * Sets the rootNode
-     * @param rootNode
-     */
-    public void setRootNode(HashNode rootNode){
-      this.rootNode= rootNode;
-    }
-    /**
-     * Returns the available number
-     */
-    public Integer getAvailableNumber()
-    {
-      return availableNumber;
-    }// getAvailableNumber()
-
-    public static void main(String[] args)
-    {
-      PrintWriter pen = new PrintWriter(System.out, true);
-      HashMap<HashNode, Integer> map = new HashMap<HashNode, Integer>();
-      Table table = new Table(map);
-
-      // Testing increment
-      pen.println("Before increment():" + table.getAvailableNumber());
-      table.incrementAvailableNum();
-      pen.println("After increment():" + table.getAvailableNumber());
-
-      
-      //Equality of HashMaps
-      HashMap<String,Integer> map2  = new HashMap<String,Integer>();
-      map2.put("a", 1);
-      map2.put("b", 2);
-      
-      HashMap<String,Integer> map4  = new HashMap<String,Integer>();
-      map4.put("a", 1);
-      map4.put("b", 2);
-      
-      pen.println(map2.equals(map4)); 
-    }
-  }// Table
+  /**
+   * Returns the available number
+   */
+  public Integer getAvailableNumber()
+  {
+    return availableNumber;
+  }// getAvailableNumber()  
+  
+  /**
+   * Returns the HashMap
+   */
+  public HashMap<HashNode,Integer> getMap(){
+    return this.map;
+  }//getMap()  
+}// Table
