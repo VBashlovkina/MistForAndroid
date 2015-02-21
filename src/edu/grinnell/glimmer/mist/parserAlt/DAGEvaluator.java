7119f484b652fd43f64f0de9ab966331bc291659
Vasilisa Bashlovkina <bashlovk@grinnell.edu>
2015-02-21 16:26:24 -0600

    stubbed evaluator for DAG

---
+++ src/edu/grinnell/glimmer/mist/parserAlt/DAGEvaluator.java
@@ -0,0 +1,60 @@
+package edu.grinnell.glimmer.mist.parserAlt;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import edu.grinnell.glimmer.mist.parser.Parser;
+import edu.grinnell.glimmer.mist.parser.TreeNode;
+
+public class DAGEvaluator
+{
+
+  
+  public static double evaluate(TreeNode root)
+  {
+    if (root.isSet())
+        return root.getEvaluation();
+
+    
+    if (root.isLeaf())
+      {
+        //STUB
+        root.set();
+        root.evaluate(1.0);
+        return 1.0; //all leaves have value 1 for now
+      }// if leaf
+    
+    ArrayList<Double> temp = new ArrayList<>();
+    for (TreeNode kid : root.getChildren())
+      {
+        temp.add(evaluate(kid));
+      }
+    root.set();
+    root.evaluate(applyFunction(root.getNodeVal(), temp));
+    return root.getEvaluation();
+  }//
+  
+  static double applyFunction(String functionName, ArrayList<Double> args)
+  {
+    //STUB - always assume the function is sum 
+    double sum = 0;
+    for (Double val : args)
+      {
+        sum += val;
+      }
+    return sum;
+  }
+  
+  public static void main(String[] args) throws Exception
+  {
+    PrintWriter pen = new PrintWriter(System.out, true);
+    String code = "sum(neg(x), neg(x), x, y)";
+    TreeNode root = Parser.parse(code);
+    pen.println("After parsing, the tree is:\n " + root);
+    TreeNode dagRoot = DAG.makeDAG(root);
+    pen.println("After making a DAG, the tree is:\n " + dagRoot);
+    System.out.println("Result is " + evaluate(dagRoot));//should be 4
+
+  }
+
+}
