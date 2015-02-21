a201661e0ab8aef1b4073af883d80c6b87b7631d
albertowusuasare <owusuasa@grinnell.edu>
2015-02-21 04:22:16 -0500

    ->Dag unit test: single test completed

---
+++ src/edu/grinnell/glimmer/mist/parserAlt/DAGTest.java
@@ -0,0 +1,19 @@
+package edu.grinnell.glimmer.mist.parserAlt;
+
+import org.junit.Test;
+
+import edu.grinnell.glimmer.mist.parser.TreeNode;
+
+public class DAGTest
+{
+/**
+ * Tests for when the root Node from non directed graph is a leaf
+ */
+  @Test
+  public void rootLeafTest()
+  {
+    TreeNode root = new TreeNode("x");
+    
+  }//rootLeafTest
+
+}
