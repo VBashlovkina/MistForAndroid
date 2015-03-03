b7b870d7758ed29fc449fabe71aba285a04ce5eb
albertowusuasare <owusuasa@grinnell.edu>
2015-03-03 01:44:51 -0600

    Set up test Files for evaluator

---
+++ src/evaluating/DAGEvaluatorTest.java
@@ -0,0 +1,13 @@
+package evaluating;
+
+import org.junit.Test;
+
+public class DAGEvaluatorTest
+{
+  @Test
+  public void testOne(){
+    
+  }
+  
+
+}
---
+++ src/evaluating/RGBValueTest.java
@@ -0,0 +1,11 @@
+package evaluating;
+
+import org.junit.Test;
+
+public class RGBValueTest
+{
+ @Test
+ public void testOne(){
+   
+ }
+}
